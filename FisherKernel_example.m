8de91d04af8caadce7d665cfafdd74cfcb5d1584
ahrends <85490363+ahrends@users.noreply.github.com>
2022-09-14 16:20:16 +0200

    Add files via upload

---
+++ FisherKernel_example.m
@@ -0,0 +1,188 @@
+%% Fisher kernel: Example
+% 
+% Examples for constructing Fisher kernel from HMM for use in prediction
+% models
+% Example 1: Gaussian HMM
+% Example 2: TDE-HMM
+% Prediction model examples: SVM, KRR
+% 
+% Christine Ahrends, Aarhus University (2022)
+
+%% set paths for data, scripts and dependencies
+projectdir = '/projects/MINDLAB2021_COMP-DecodingTempGenMat';
+scriptdir = [projectdir '/scripts/Kernel/FK_github'];
+hmm_scriptdir = [projectdir '/scripts/HMM-MAR-master'];
+datadir = [projectdir '/scratch/Kernel/data'];
+
+cd(projectdir)
+addpath(scriptdir)
+addpath(genpath(hmm_scriptdir));
+
+%% load data (X: timecourses, Y: behavioural variable to be predicted)
+
+% load X
+load([datadir '/example_data.mat'], 'X');
+% X are the timecourses that the HMM will be run on
+% assuming here that timecourses are a subjects x 1 cell, each containing a
+% timepoints x ROIs matrix
+
+% load Y 
+load([datadir '/example_data.mat'], 'Y');
+nvars = size(Y,2);
+% Y are the variables to be predicted
+% should be a subjects x variables matrix
+
+S = size(X,1);
+N = size(X{1},2);
+T = cell(S,1);
+for s = 1:S
+    T{s} = size(X{s},1);
+end
+
+%% Example 1: Gaussian HMM with mean and covariance
+
+hmm_options1 = struct();
+hmm_options1.order = 0;
+hmm_options1.covtype = 'full'; %('full' for covariance, 'uniquefull' for no covariance)
+hmm_options1.zeromean = 0; % (0 to model mean, 1 to model only covariance)
+hmm_options1.standardise = 1; 
+hmm_options1.dropstates = 0;
+hmm_options1.K = 6;
+hmm_options1.useParallel = 0;
+
+% run HMM (group-level)
+[HMM1.hmm, HMM1.Gamma, HMM1.Xi, HMM1.vpath, ~, ~, HMM1.fehist] = hmmmar(X, T, hmm_options1);
+
+%% construct kernels and feature matrices from HMM
+
+% at the moment: use either a) Pi & P, b) Pi, P, and sigma, or c) Pi, P, mu, and sigma
+K_options = struct();
+K_options.Pi = true; % state probabilities
+K_options.P = true; % transition probabilities
+K_options.mu = true; % state means
+K_options.sigma = true; % state covariances
+K_options.type = 'gradient'; % one of 'vectorised', 'vectorised_norm', or 'gradient' (use gradient for Fisher kernel)
+K_options.kernel = 'linear';
+K_options.normalisation = 'L2-norm'; % only here for visualisation, drop this when running KRR
+
+[FK, feat] = hmm_kernel(X, HMM1.hmm, K_options);
+% FK is the Fisher kernel
+% figure; subplot(1,2,1); imagesc(feat); title('Gradient features (normalised)'); 
+% xlabel('Features'); ylabel('Subjects'); colorbar; 
+% subplot(1,2,2); imagesc(FK); title('Fisher Kernel (normalised)'); 
+% xlabel('Subjects'); ylabel('Subjects'); axis square; colorbar;
+
+% alternatively, use Gaussian Fisher kernel:
+K_options.kernel = 'Gaussian';
+K_options.tau = 100; % estimate tau in CV
+
+[FKg, feat] = hmm_kernel(X, HMM1.hmm, K_options);
+% FKg is the Gaussian version of the Fisher Kernel
+% figure; subplot(1,2,1); imagesc(feat); title('Gradient features (normalised)'); 
+% xlabel('Features'); ylabel('Subjects'); colorbar; 
+% subplot(1,2,2); imagesc(FKg); title('Gaussian Fisher Kernel (normalised)'); 
+% xlabel('Subjects'); ylabel('Subjects'); axis square; colorbar;
+
+%% Example 2: TDE-HMM
+
+hmm_options2 = struct();
+hmm_options2.order = 0;
+hmm_options2.covtype = 'full'; %('full' for covariance, 'uniquefull' for no covariance)
+hmm_options2.standardise = 1; 
+hmm_options2.dropstates = 0;
+hmm_options2.K = 6;
+hmm_options2.useParallel = 0;
+% options specific for TDE-HMM:
+hmm_options2.embeddedlags = -7:7;
+hmm_options2.pca = 0.5; % just for illustration, this should be closer to 2*N_regions
+hmm_options2.zeromean = 1; % needs to be 1 for TDE-HMM
+
+% run HMM (group-level)
+[HMM2.hmm, HMM2.Gamma, HMM2.Xi, HMM2.vpath, ~, ~, HMM2.fehist] = hmmmar(X, T, hmm_options2);
+
+% gradient works in embedded space, check transformed data:
+% [Xt_tmp, Tt] = transform_dataHMM(X,T, hmm_options2);
+% 
+%% construct kernels and feature matrices from HMM
+
+% at the moment: use either a) Pi & P, b) Pi, P, and sigma, or c) Pi, P, mu, and sigma
+K_options = struct();
+K_options.Pi = true; % state probabilities
+K_options.P = true; % transition probabilities
+K_options.mu = false; % state means
+K_options.sigma = true; % state covariances
+K_options.type = 'gradient'; % one of 'vectorised', 'vectorised_norm', or 'gradient' (use gradient for Fisher kernel)
+K_options.kernel = 'linear';
+K_options.normalisation = 'L2-norm'; % only here for visualisation, drop this when running KRR
+
+[FK2, feat2] = hmm_kernel(X, HMM2.hmm, K_options);
+% FK is the Fisher kernel
+% figure; subplot(1,2,1); imagesc(feat2); title('Gradient features (normalised)'); 
+% xlabel('Features'); ylabel('Subjects'); colorbar; 
+% subplot(1,2,2); imagesc(FK2); title('Fisher Kernel (normalised)'); 
+% xlabel('Subjects'); ylabel('Subjects'); axis square; colorbar;
+
+% NOTE: gradient features will be in embedded space (e.g. embedded lags & 
+% PCA space)
+
+% alternatively, use Gaussian Fisher kernel:
+K_options.kernel = 'Gaussian';
+K_options.tau = 100; % estimate tau in CV
+
+[FKg2, feat2] = hmm_kernel(X, HMM2.hmm, K_options);
+% FKg is the Gaussian version of the Fisher Kernel
+% figure; subplot(1,2,1); imagesc(feat2); title('Gradient features (normalised)'); 
+% xlabel('Features'); ylabel('Subjects'); colorbar; 
+% subplot(1,2,2); imagesc(FKg2); title('Gaussian Fisher Kernel (normalised)'); 
+% xlabel('Subjects'); ylabel('Subjects'); axis square; colorbar;
+
+
+%% Prediction example 1: SVM
+
+K_options.normalisation = []; 
+K_options.tau = [];
+[FK, feat] = hmm_kernel(X, HMM1.hmm, K_options);
+
+% split into training and test set
+rng('shuffle')
+ind = 1:S;
+split = 0.8; % ratio to split data into training and test set (variable is training set percentage)
+train_size = round(S*split);
+test_size = S-train_size;
+[train_ind] = randsample(ind, train_size);
+%size(unique(train_ind)) % check that the sampling picked (split) percentage of unique values from the dataset
+test_ind = ind(~ismember(ind, train_ind));
+train_feat = feat(train_ind,:);
+test_feat = feat(test_ind,:);
+Y_train = Y(train_ind,1); %binary variable
+Y_test = Y(test_ind,1);
+
+% fit SVM to predict binary variable in test set from gradient features
+svm1 = fitcsvm(train_feat, Y_train, 'Standardize', false, 'KernelFunction', 'linear'); % linear kernel on the gradient features will essentially give the practical Fisher kernel
+[est_labels, score] = predict(svm1, test_feat);
+err1 = sum(est_labels ~= Y_test) ./numel(Y_test);
+disp(['Generalization error on test set (Fisher kernel): ' num2str(err1)]);
+
+%% Prediction example 2: kernel ridge regression
+% set parameters for KRR
+% niter = 1;
+krr_options = struct();
+krr_options.deconfounding = 0;
+krr_options.CVscheme = [10 10];
+% krr_params.alpha = [0.0001 0.001 0.01 0.1 0.3 0.5 0.7 0.9 1.0];
+krr_options.verbose = 1;
+%krr_params.sigmafact = 1;
+krr_options.Nperm = 1; % (only relevant for permutation-based significance testing)
+krr_options.kernel = 'linear'; % krr_predict_FK uses feature matrices as input and is inefficient for Gaussian kernel (use krr_predict with distance matrices as input instead)
+
+varN = 2;
+Yin = Y(:,varN);
+index = ~isnan(Yin);
+Yin = Yin(index,:);
+Xin = feat(index,:);
+
+%disp(['Now running iteration ' num2str(i) ' out of ' num2str(niter) ...
+%    ' for variable ' num2str(j) ' out of ' num2str(nvars) ' for FK']);
+[results.predictedY, results.stats] = ...
+    krr_predict_FK(Yin,Xin, krr_options, twins(index,index)); %twins is optional family structure (CV set up to randomise folds taking family structure into account)
+disp(['Prediction accuracy (Fisher kernel) is ' num2str(results.stats.corr)])
---
+++ utils/transform_dataHMM.m
@@ -0,0 +1,110 @@
+function [data, T] = transform_dataHMM(data, T, options)
+
+if iscell(T)
+    if size(T,1)==1, T = T'; end
+    for i = 1:length(T)
+        if size(T{i},1)==1, T{i} = T{i}'; end
+    end
+end
+N = length(T);
+
+if xor(iscell(data),iscell(T)), error('X and T must be cells, either both or none of them.'); end
+
+% data can be a cell or a matrix
+if isstruct(data) && isfield(data,'C') && ...
+        isfield(options,'nessmodel') && options.nessmodel
+    warning('data.C and options.nessmodel are not compatible')
+    data = data.X;
+end
+if iscell(T)
+    T = cell2mat(T);
+end
+checkdatacell;
+
+[options,data] = checkoptions(options,data,T,0);
+checkData(data,T,options);
+
+% Standardise data and control for ackward trials
+    valid_dims = computeValidDimensions(data,options);
+    data = standardisedata(data,T,options.standardise,valid_dims); 
+    % Filtering
+    if ~isempty(options.filter)
+       data = filterdata(data,T,options.Fs,options.filter);
+    end
+    % Detrend data
+    if options.detrend
+       data = detrenddata(data,T); 
+    end
+    % Leakage correction
+    if options.leakagecorr ~= 0 
+        data = leakcorr(data,T,options.leakagecorr);
+    end
+    % Hilbert envelope
+    if options.onpower
+       data = rawsignal2power(data,T); 
+    end
+    % Leading Phase Eigenvectors 
+    if options.leida
+        data = leadingPhEigenvector(data,T);
+    end
+    % pre-embedded PCA transform
+    if length(options.pca_spatial) > 1 || (options.pca_spatial > 0 && options.pca_spatial ~= 1)
+        if isfield(options,'As')
+            data.X = bsxfun(@minus,data.X,mean(data.X));   
+            data.X = data.X * options.As; 
+        else
+            [options.As,data.X] = highdim_pca(data.X,T,options.pca_spatial);
+            options.pca_spatial = size(options.As,2);
+        end
+    end    
+    % Embedding
+    if length(options.embeddedlags) > 1  
+        [data,T] = embeddata(data,T,options.embeddedlags);
+        elmsg = '(embedded)';
+    else
+        elmsg = ''; 
+    end
+    % PCA transform
+    if length(options.pca) > 1 || (options.pca > 0 && options.pca ~= 1) || ...
+            isfield(options,'A')
+        if isfield(options,'A')
+            data.X = bsxfun(@minus,data.X,mean(data.X));   
+            data.X = data.X * options.A; 
+        else
+            [options.A,data.X,e] = highdim_pca(data.X,T,options.pca,0,0,0,options.varimax);
+            options.pca = size(options.A,2);
+            if options.verbose
+                if options.varimax
+                    fprintf('Working in PCA/Varimax %s space, with %d components. \n',elmsg,options.pca)
+                    fprintf('(explained variance = %1f)  \n',e(options.pca))
+                else
+                    fprintf('Working in PCA %s space, with %d components. \n',elmsg,options.pca)
+                    fprintf('(explained variance = %1f)  \n',e(options.pca))
+                end
+            end
+        end
+        % Standardise principal components and control for ackward trials
+        data = standardisedata(data,T,options.standardise_pc);
+        options.ndim = size(options.A,2);
+        options.S = ones(options.ndim);
+        options.Sind = formindexes(options.orders,options.S);
+        if ~options.zeromean, options.Sind = [true(1,size(options.Sind,2)); options.Sind]; end
+    else
+        options.ndim = size(data.X,2);
+    end
+    % Downsampling
+    if options.downsample > 0 
+       [data,T] = downsampledata(data,T,options.downsample,options.Fs); 
+    end
+    if options.pcamar > 0 && ~isfield(options,'B')
+        % PCA on the predictors of the MAR regression, per lag: X_t = \sum_i X_t-i * B_i * W_i + e
+        options.B = pcamar_decomp(data,T,options);
+    end
+    if options.pcapred > 0 && ~isfield(options,'V')
+        % PCA on the predictors of the MAR regression, together: 
+        % Y = X * V * W + e, where X contains all the lagged predictors
+        % So, unlike B, V draws from the temporal dimension and not only spatial
+        options.V = pcapred_decomp(data,T,options);
+    end   
+    
+end
